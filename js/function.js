f7ea74d37239ffc7e06572e95515c8279a1f51f0
Hoang Hieu <sunvni@gmail.com>
2021-05-18 19:45:48 +0900

    initial

--- js/function.js
+++ js/function.js
@@ -1,15 +1,24 @@
 
-window.onload = loadList
+window.onload = loadRules
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('jsAddBtn').addEventListener('click', () => {
         addItem();
-        loadList();
+        loadRules();
+    })
+    showMore.addEventListener("click", function () {
+        if (jsDomain.className == "active") {
+            jsDomain.className = ""
+            jsRedirect.className = ""
+        } else {
+            jsDomain.className = "active"
+            jsRedirect.className = "active"
+        }
     })
 })
 
-function removeItem(index) {
-    let data = localStorage.getItem('url_list');
+function removeRule(index) {
+    let data = localStorage.getItem('rules');
 
     let urlList = JSON.parse(data);
 
@@ -18,47 +27,68 @@ function removeItem(index) {
     }
 
     urlList.splice(index, 1)
-    saveList(urlList)
+    saveRule(urlList)
 }
 
 function addItem() {
-    let newUrl = document.querySelector('#jsNewUrl').value
+    let jsQuery = document.querySelector('#jsQuery').value
+    let jsDomain = document.querySelector('#jsDomain').value
+    let jsRedirect = document.querySelector('#jsRedirect').value
 
-    document.querySelector('#jsNewUrl').value = ""
 
-    let data = localStorage.getItem('url_list');
 
-    let urlList = JSON.parse(data) || [];
-
-    if (!~urlList.indexOf(newUrl)) {
-        urlList.push(newUrl);
+    let rule = {
+        "priority": 1,
+        "action": {
+            "type": "redirect",
+            "redirect": {
+                "url": jsRedirect
+            }
+        },
+        "condition": {
+            "urlFilter": jsQuery,
+            "domains": [
+                jsDomain
+            ],
+            "resourceTypes": [
+                "main_frame"
+            ]
+        }
     }
-    saveList(urlList)
+
+    let data = localStorage.getItem('rules');
+
+    let rules = JSON.parse(data) || [];
+
+    rules.push(rule);
+
+    saveRule(rules)
 }
 
 
-function loadList() {
-    let data = localStorage.getItem('url_list');
+function loadRules() {
+    let data = localStorage.getItem('rules');
 
-    let urlList = JSON.parse(data) || [];
+    let rules = JSON.parse(data) || [];
 
-    jsUrlList.innerHTML = urlList.map((item, index) => {
+    jsUrlList.innerHTML = rules.map((item, index) => {
         return `
-            <li><p>${item}</p><span class="remove" data-index="${index}">x</span></li>
+            <li><p>${item.condition.urlFilter}</p><span class="remove" data-index="${index}">x</span></li>
             `
     }).join('\n')
 
     let elements = document.getElementsByClassName('remove')
+
     for (let el of elements) {
-        el.addEventListener('click', function(e) {
+        el.addEventListener('click', function (e) {
             let target = e.target
             let index = target.dataset['index']
-            removeItem(index)
-            loadList()
+            removeRule(index)
+            loadRules()
         })
     }
 }
 
-function saveList(list) {
-    localStorage.setItem('url_list', JSON.stringify(list))
+function saveRule(rules) {
+    localStorage.setItem('rules', JSON.stringify(rules))
 }
